30803a23e0970b8ec6907d0bfe2db7219c620d41
j-rods <jrrodriguez84@gmail.com>
2017-02-22 11:00:36 +0000

    Delete ex11b.rb

--- ex11b.rb
+++
@@ -1,9 +0,0 @@
-#form 2
-puts "Hi! What's your name? "
-name = gets.chomp
-
-puts "Hi " + name + "!"
-puts "What's your favourite food?"
-favfood = gets.chomp
-
-puts "Oh wow " + name + ", " + favfood + " is also my favourite!" 
