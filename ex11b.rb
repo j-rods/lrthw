a11a8cfeea301af7fad974125e2b80eae27f65cf
Jessica Rodriguez <jessicarodriguez@Jessicas-MacBook-Pro.local>
2017-02-22 09:33:20 +0000

    ex11 form exercise 2

---
+++ ex11b.rb
@@ -0,0 +1,9 @@
+#form 2
+puts "Hi! What's your name? "
+name = gets.chomp
+
+puts "Hi " + name + "!"
+puts "What's your favourite food?"
+favfood = gets.chomp
+
+puts "Oh wow " + name + ", " + favfood + " is also my favourite!" 
