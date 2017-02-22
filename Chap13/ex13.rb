b929fb3b9c6e76e12d11161bdeb1e7fb0feb3be8
Jessica Rodriguez <jessicarodriguez@Jessicas-MacBook-Pro.local>
2017-02-22 11:28:49 +0000

    Parameters, Unpacking, Variables

---
+++ Chap13/ex13.rb
@@ -0,0 +1,8 @@
+first, second, third = ARGV
+
+puts "Your first variable is: #{first}"
+puts "Your second variable is: #{second}"
+puts "Your third variables is: #{third}"
+
+# to run:
+# $ ruby ex13.rb first 2nd 3rd
---
+++ Chap13/ex13b.rb
@@ -0,0 +1,19 @@
+#To run, type in command ruby ex13b apple kinderbueno
+
+
+fav_fruit, fav_chocolate = ARGV
+
+#outputs the first argument which is fruit
+puts "Your favourite fruit is #{fav_fruit}, but I prefer mangoes."
+#outputs the second argument which is kinderbueno
+puts "Your favourite chocolate is #{fav_chocolate}, hmm I like that."
+
+#changes first and second argument for 2 new arguments given with gets
+print "do you have any other favourite fruit? "
+fav_fruit = $stdin.gets.chomp
+print "and any other favourite chocolate? "
+fav_chocolate = $stdin.gets.chomp
+
+#outputs the 2 new arguments, replacing the initial 2 used to run the program
+puts "Oh! I also like #{fav_fruit}! But I'm not really into #{fav_chocolate}..."
+
