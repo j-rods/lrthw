5f000c5e23bfc59457e82a4394d9aedfa6d6f7e0
j-rods <jrrodriguez84@gmail.com>
2017-02-23 09:05:56 +0000

    Update ex3.rb

--- Chap03/ex3.rb
+++ Chap03/ex3.rb
@@ -4,25 +4,25 @@ puts "I will now count my chickens:"
 #returns the result of a maths operation that includes a sum and division
 puts "Hens #{25.0 + 30.0 / 6.0}"
 
-#returns the result of a maths operation that includes a substracton, multiplication and a percentage
+#returns the result of a maths operation that includes a subtracton, multiplication and a percentage
 puts "Roosters #{100.0 - 25.0 * 3.0 % 4.0}"
 
 #string explains that there will be a calculation returned below
 puts "Now I will count the eggs:"
 
-#returns the result of a maths operation that includes sums, substraction, percentage and division
+#returns the result of a maths operation that includes sums, subtraction, percentage and division
 puts 3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0
 
 #compares if 3 + 2 is less than 5 - 7 and if it is, returns true, else returns false (expected false)
 puts "Is it true that 3.0 + 2.0 < 5.0 - 7.0?"
 
-#returns the result of sum 3 + 2 and compares if it's less than the result of the substraction 5 - 7
+#returns the result of sum 3 + 2 and compares if it's less than the result of the subtraction 5 - 7
 puts 3.0 + 2.0 < 5.0 - 7.0
 
 #asks a question to a sum calculation and returns that calculation
 puts "What is 3 + 2? #{3.0 + 2.0}"
 
-#asks a question to a substraction and returns that calculation
+#asks a question to a subtraction and returns that calculation
 puts "What is 5 - 7? #{5.0 - 7.0}"
 
 #returns an understanding of why the above comparison returned false
