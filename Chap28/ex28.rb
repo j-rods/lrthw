1b1f7aab293fc7a6aa132bf491cde51acc870dce
Jessica Rodriguez <jessicarodriguez@Jessicas-MacBook-Pro.local>
2017-02-23 17:05:35 +0000

    Boolean practice

---
+++ Chap28/ex28.rb
@@ -0,0 +1,85 @@
+# true
+# and
+true && true
+
+# false
+false && true
+
+# false
+1 == 1 && 2 == 1
+
+# true
+"test" == "test"
+
+# true
+1 == 1 || 2 != 1
+
+# true
+true && 1 == 1
+
+# false
+false && 0 != 0
+
+# true
+true || 1 == 1
+
+# false
+"test" == "testing"
+
+# false
+1 != 0 && 2 == 1
+
+# true
+"test" != "testing"
+
+# false
+"test" == 1
+
+# true
+!(true && false)
+
+# false
+!(1 == 1 && 0 != 1)
+
+# false
+!(10 == 1 || 1000 == 1000)
+
+# false
+!(1 != 10 || 3 == 4)
+
+# true
+!("testing" == "testing" && "Zed" == "Cool Guy")
+
+# true
+1 == 1 && (!("testing" == 1 || 1 == 0))
+
+# false
+"chunky" == "bacon" && (!(3 == 4 || 3 == 3))
+
+# false
+3 == 3 && (!("testing" == "testing" || "Ruby" == "Fun"))
+
+# false
+# more than
+10 > 10000
+
+# false
+# less than
+45 < -5
+
+# false
+# less than or equal to
+10 <= 5
+
+# true
+# less than or equal to
+5 <= 10
+
+# true
+# less than or equal to
+5 <= 5
+
+
+
+
+
